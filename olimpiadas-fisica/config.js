8c76619af00ea65fbaaabf61dbdfa38b6a99bfa8
juanperez238421-cpu <juanperez238421@gmail.com>
2026-09-01 07:44:14 -0500

    Add Physics Olympiad Hub Supabase config

---
+++ olimpiadas-fisica/config.js
@@ -0,0 +1,6 @@
+window.PHYSICS_OLYMPIAD_CONFIG = Object.freeze({
+  projectUrl: "https://rlfxnjbqxbozjdzkbwlz.supabase.co",
+  publishableKey: "sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87",
+  functionName: "physics-olympiad-hub",
+  storageKey: "ijr.physicsOlympiad.accessToken.v1"
+});
